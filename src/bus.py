f3fa3624c0a0234798f07ddecc43580e69534f42
Katie Strong <katiestrong123@gmail.com>
2020-11-10 16:09:22 +0000

    starts_with_no_passengers

--- src/bus.py
+++ src/bus.py
@@ -2,6 +2,10 @@ class Bus:
     def __init__(self, route_number, destination):
         self.route_number = route_number
         self.destination = destination
+        self.passengers = 0
     
     def drive(self):
         return "Brum brum"
+
+    def passenger_count(self):
+        return self.passengers
